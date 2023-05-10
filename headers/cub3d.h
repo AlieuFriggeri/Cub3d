0c8aec19becd0be0130fd44279863cc25b500ccd
vgroux <vgroux@student.42lausanne.ch>
2023-05-10 15:24:48 +0200

    norm headers

--- headers/cub3d.h
+++ headers/cub3d.h
@@ -6,26 +6,26 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 11:13:12 by afrigger          #+#    #+#             */
-/*   Updated: 2023/05/10 15:18:07 by vgroux           ###   ########.fr       */
+/*   Updated: 2023/05/10 15:24:32 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
-#define	CUB3D_H
+# define CUB3D_H
 
-#include <math.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include "../mlx_macos/mlx.h"
-#include "../libft/libft.h"
+# include <math.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include "../mlx_macos/mlx.h"
+# include "../libft/libft.h"
 
-#define WIDTH 1024
-#define HEIGHT 512
-#define PI 3.1415926535
-#define DEG 0.0174533
+# define WIDTH 1024
+# define HEIGHT 512
+# define PI 3.1415926535
+# define DEG 0.0174533
 # define BLUE_COLOR 0x87CEEB
 # define GREY_COLOR 0x808080
 
@@ -55,7 +55,6 @@ typedef struct s_cub{
 	int			mapx;
 	int			mapy;
 }	t_cub;
-
 
 /* ----- MAIN ----- */
 int		main(void);
--- headers/hook.h
+++ headers/hook.h
@@ -3,22 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   hook.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afrigger <afrigger@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 12:22:09 by afrigger          #+#    #+#             */
-/*   Updated: 2023/03/16 15:19:56 by afrigger         ###   ########.fr       */
+/*   Updated: 2023/05/10 15:24:23 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HOOK_H
-#define HOOK_H
+# define HOOK_H
 
-#include "cub3d.h"
+# include "cub3d.h"
 
 // hook.c
 int		hook(int keycode, t_cub *data);
 int		cub_exit(t_cub *data);
-void	move_player(int keycode ,t_cub *data);
+void	move_player(int keycode, t_cub *data);
 void	rotate_player(int keycode, t_cub *data);
 
 #endif
